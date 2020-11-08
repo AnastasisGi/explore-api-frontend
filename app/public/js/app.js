97b8c41750f19b0764a8c5bff78d5f3c4047f624
Edward Withers <dearshrewdwit@gmail.com>
2020-11-08 09:33:27 +0000

    Update app.js

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,7 +1,11 @@
 // when the window loads, load the front end app.
 
 window.addEventListener('load', () => {
+  console.log("window loaded")
   element = document.getElementById('app')
+  console.log("app element:", element)
   weather = new Weather($, element)
+  console.log("weather:", weather)
   weather.render()
+  console.log("app loaded")
 })
