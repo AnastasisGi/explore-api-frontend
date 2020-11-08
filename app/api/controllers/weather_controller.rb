3852e43f9c2f8d5d8fcf8e3987ebed3791202556
Edward Withers <dearshrewdwit@gmail.com>
2020-11-08 09:35:53 +0000

    Update weather_controller.rb

--- app/api/controllers/weather_controller.rb
+++ app/api/controllers/weather_controller.rb
@@ -1,7 +1,9 @@
 class WeatherApi < Sinatra::Base
   namespace '/api' do
     get '/current_weather' do
+      puts 'at the endpoint "/current_weather"'
       data = File.read('data.json')
+      puts data
       status 200
 
       json data
