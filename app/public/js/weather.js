390f1e374559d663147d51a3702e4136b8a9f352
Edward Withers <dearshrewdwit@gmail.com>
2020-11-08 09:30:56 +0000

    Add logs

--- app/public/js/weather.js
+++ app/public/js/weather.js
@@ -2,14 +2,19 @@
 
 class Weather {
   constructor($, element) {
+    console.log("Weather class is instantiated")  
     this.$ = $
     this.element = element
   }
 
   render() {
+    console.log("render function called")
     this.$.get('/api/current_weather')
       .then(data => {
+        console.log("In jQuery 'get' callback")
+        console.log("data: ", data)
         let parsed_data = JSON.parse(data)
+        console.log("parsed_data: ", parsed_data)
         this.element.innerHTML = `<h1>Current weather temperature: ${parsed_data.main.temp} Kelvin</h1>`
       })
   }
